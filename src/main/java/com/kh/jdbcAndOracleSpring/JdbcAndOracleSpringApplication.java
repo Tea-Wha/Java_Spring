70adc7dda81be6d358e34e8c970686baebb81450
Tea-Wha <htw7880@gmail.com>
2024-10-23 09:16:04 +0900

    Java Spring_Project

---
+++ src/main/java/com/kh/jdbcAndOracleSpring/JdbcAndOracleSpringApplication.java
@@ -0,0 +1,28 @@
+package com.kh.jdbcAndOracleSpring;
+
+import com.kh.jdbcAndOracleSpring.dao.EmpDAO;
+import com.kh.jdbcAndOracleSpring.vo.EmpVO;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.List;
+
+@SpringBootApplication
+public class JdbcAndOracleSpringApplication implements CommandLineRunner {
+	private final EmpDAO dao;
+
+	public JdbcAndOracleSpringApplication(EmpDAO dao){
+		this.dao = dao;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdbcAndOracleSpringApplication.class, args);
+	}
+
+	@Override // 터미널 모드 관련된 코드는 이곳에
+	public void run(String... args) throws Exception {
+		List<EmpVO> list = dao.empSelect();
+		dao.empSelectResult(list);
+	}
+}
